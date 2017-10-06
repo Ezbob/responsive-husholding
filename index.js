7ed0c9ce5e827e9bbad0d3ee8e8dd2796e75aa0f
Tiggles <jonas@hinchely.dk>
2017-10-06 21:40:21 +0200

    Addition spelling fix and switch to fs

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
+var fs = require('fs');
 
 const app = express();
 
 app.get('/', function(req, res) {
-	res.send("hello there");
+	if (req.method === 'GET') {
+        fs.readFile("index.html", function( err, data ) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+        });
+    } else {
+    	console.log("I'm not implemented.");
+    }
 });
 
 
+
 app.listen(8080, function() {
-	console.log("Listening on port 80");
+	console.log("Listening on port 8080");
 });
 
