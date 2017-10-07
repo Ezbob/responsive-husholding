7ac5dac99e3aeb44880c94622128c1be16692395
Ezbob <andersbusch@gmail.com>
2017-10-07 16:53:02 +0200

    moved stuff around

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join( app.get('views_path'), 'index.html'));
 });
 
+
+
 app.use(function(err, req, res, next) {
 	console.error(err.stack);
 	res.status(500).send("<h1>Sweet burn!!</h1>");
