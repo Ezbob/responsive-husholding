26d44147b6548fb35058b45b4817f4987decbd8e
Ezbob <andersbusch@gmail.com>
2017-10-08 20:55:57 +0200

    create form, an outline of a restful api for grossery listing

--- index.js
+++ index.js
@@ -4,21 +4,52 @@ const path = require('path');
 
 const app = express();
 
+// setting paths and variable available from the app 
 app.set('root_path', path.resolve(__dirname));
 app.set('views_path', path.join(app.get('root_path'), 'views'));
 app.set('database', path.join(app.get('root_path'), 'database.sqlite'));
 
+// use the pug template engine to render views
+app.set('views', app.get('views_path'));
+app.set('view engine', 'pug'); 
+
+// a Knex instance hooked up to the database
 const database = require("./modules/database.js")(app);
 
 app.use('/static', express.static('assets'));
 
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
 
 	res.sendFile(path.join( app.get('views_path'), 'index.html'));
 });
 
-app.use(function(err, req, res, next) {
+app.route('/grosslist/')
+	.get((req, res) => {
+		// get the whole list
+	})
+	.post((req, res) => {
+		// create a item (let the database sort out the id)
+	});
+
+app.route('/grosslist/:itemId')
+	.get((req, res) => {
+		// get a specific item with id
+	})
+	.put((req, res) => {
+		// update a specific item
+	})
+	.delete((req, res) => {
+		// delete a specific item
+	})
+
+
+app.use((req, res, next) => {
+	console.error("NOT FOUND " + req.method + " " + req.protocol + " " + req.url);
+	res.status(404).send("<p>Not found</p>");
+})
+
+app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.status(500).send("<h1>Sweet burn!!</h1>");
 });
